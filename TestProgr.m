5ceaf867d3add4478ded17df252288a8df93ea57
WilliamKulp <49132740+WilliamKulp@users.noreply.github.com>
2019-12-01 11:09:58 -0500

    added a max iterations attribute to angular rmse learning

--- TestProgr.m
+++ TestProgr.m
@@ -1,7 +1,7 @@
 Learn = load('arithm_prog_learn.txt');
 Test = load('arithm_prog_test.txt');
 
-Results = MLMVN('sizeOfMlmvn', [2 1], 'inputs', Learn, 'stoppingCriteria', 'rmse', 'discreteInput', 1, 'discreteOutput', 1, 'globalthresholdvalue', 0, 'localThresholdValue', 0, 'SoftMargins', 1, 'angularGlobalThresholdValue', 0.001, 'angularLocalThresholdValue', 0,'initialWeights','random', 'numberOfSectors', 64);
+Results = MLMVN('sizeOfMlmvn', [2 1], 'inputs', Learn, 'stoppingCriteria', 'rmse', 'discreteInput', 1, 'discreteOutput', 1, 'globalthresholdvalue', 0, 'localThresholdValue', 0, 'SoftMargins', 1, 'angularGlobalThresholdValue', 0.001, 'angularLocalThresholdValue', 0,'initialWeights','random', 'numberOfSectors', 64, 'maxIterations', 100);
 Weights = Results.network;
 Prediction = MLMVN('network', Weights, 'inputs', Test, 'stoppingCriteria', 'test', 'discreteInput', 1, 'discreteOutput', 1, 'globalthresholdvalue', 0.1, 'localThresholdValue', 0, 'numberOfSectors', 64);
 disp('Desired Outputs');
